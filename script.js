2bf947886d1ba836a8ec2ab736bad0d2663dd690
Ankit <33988371+dankit24@users.noreply.github.com>
2019-12-12 18:29:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ console.log(status+" and "+toastMessage);
   createToastHeader(status, toastContainer);
   createToastContent(toastContainer, toastMessage);
   initToast(toastContainer);
-  setTimeout(function(){ destroyToast(toastContainer); },5000) ;
+  // setTimeout(function(){ destroyToast(toastContainer); },5000) ;
 }
 
 function createToastContainer(status) {
+  console.log("creating container");
   var toastContainer = document.createElement("div");
   toastContainer.setAttribute("id","toast");
    toastContainer.setAttribute("onclick","this.remove();");
@@ -25,6 +26,7 @@ function createToastContainer(status) {
 }
 
 function createToastHeader(status, toastContainer){
+  console.log("creating toast header");
   var toastHeader = document.createElement("div");
   if (status == "success") {
     toastHeader.className = "toastContainerSuccess";
@@ -37,10 +39,12 @@ function createToastHeader(status, toastContainer){
   }
   toastHeader.setAttribute("id","toastHeader");
   toastHeader.appendChild(document.createTextNode(status.toUpperCase()));
+  toastHeader.className = "div-table-row";
   toastContainer.appendChild(toastHeader);
 }
 
 function createToastContent(toastContainer, toastMessage){
+  console.log("creating toast content");
   var toastContent = document.createElement("div");
   toastContent.setAttribute("id","toastContent");
   toastContent.appendChild(document.createTextNode(toastMessage));
@@ -48,10 +52,25 @@ function createToastContent(toastContainer, toastMessage){
 }
 
 function initToast(toastContainer){
-  document.body.appendChild(toastContainer);
+  console.log("initialising toast");
+  if(document.getElementsByTagName("main").length === 0 && document.getElementsByTagName("app-login").length !== 0){
+    console.log("main tag not found nut app-login found");
+    var mainelement = document.getElementsByTagName("app-login")[0];
+    mainelement.appendChild(toastContainer);
+  }
+  else if(document.getElementsByTagName("main").length !== 0){
+    console.log("main tag found");
+  var mainelement = document.getElementsByTagName("main")[0];
+  mainelement.appendChild(toastContainer);
+  }
+  else{
+    console.log("appending in body");
+    document.body.appendChild(toastContainer);
+  }
 }
 
 function destroyToast(toastContainer){
+  console.log("destroying toast");
   if(document.body.contains(toastContainer)){
   document.getElementById("toast").remove();
   }
